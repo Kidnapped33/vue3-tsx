872c4105f2c9bcafd143a5d70ad090b115d51d5b
ashine33 <ashine33@foxmail.com>
2022-05-17 17:46:12 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: "/", //本地部署 //或者是github /仓库名称/
+});

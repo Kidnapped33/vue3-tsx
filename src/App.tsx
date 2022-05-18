45ef835482b8ea09784e7ab93848051bca4a41b8
ashine33 <ashine33@foxmail.com>
2022-05-18 14:18:49 +0800

    使用tsx语法

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { defineComponent, ref } from "vue";
+
+export const App = defineComponent({
+  setup() {
+    const count = ref(0);
+    const add = () => {
+      count.value += 1;
+    };
+    return () => (
+      <>
+        <div>{count.value}</div>
+        <button onClick={add}>++++</button>
+      </>
+    );
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/mangosteen-fe-1/dist/", //本地部署 //或者是github /仓库名称/
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true,
+    }),
+  ],
 });

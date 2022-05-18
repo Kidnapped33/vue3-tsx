ede4515e4b7e3b5e1805d1be5a8f2e52469e860d
ashine33 <ashine33@foxmail.com>
2022-05-18 10:36:27 +0800

    删除无用的代码+创建文件夹+.keep

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "/mangosteen-fe-1/dist/", //本地部署 //或者是github /仓库名称/
   plugins: [vue()],
-  base: "/", //本地部署 //或者是github /仓库名称/
 });

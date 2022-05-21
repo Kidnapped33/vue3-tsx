d0b6a2fc2c2e4b2000daa360b56751092e6ef82e
ashine33 <ashine33@foxmail.com>
2022-05-21 18:20:36 +0800

    feat：初始化4个欢迎页

--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,10 +1,21 @@
 import { defineComponent } from "vue";
+import { RouterLink } from "vue-router";
+import s from "./First.module.scss";
+import pig from "../../assets/icons/watermelon.svg";
 
 export const Second = defineComponent({
   setup() {
     return () => (
-      <div>
-        Second
+      <div class={s.wapper}>
+       <div class={s.card}>
+         <img class={s.pig} src={pig}></img>
+         <h2>第二只<br/>还要会省钱</h2>
+       </div>
+       <div  class={s.actions}>
+         <RouterLink to="" class={s.fake}>跳过</RouterLink>
+         <RouterLink to="/welcome/third">下一页</RouterLink>
+         <RouterLink to="/start">跳过</RouterLink>
+       </div>
       </div>
     );
   },

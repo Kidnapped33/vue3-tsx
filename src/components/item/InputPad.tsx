c590b25863197371740c1b77863235a1f5913a6f
Kidnapped33 <404770266@qq.com>
2022-08-23 10:20:57 +0800

    添加time.tsx,后续使用vant ui

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,6 +1,7 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { Icon } from "../../shared/Icon";
 import s from "./InputPad.module.scss";
+import { time } from '../../shared/time';
 
 export const InputPad = defineComponent({
   props: {
@@ -9,37 +10,44 @@ export const InputPad = defineComponent({
     },
   },
   setup: () => {
+    const refDate = ref<Date>();
+    const now = new Date();
     const buttons = [
-        { text:'1', onClick: () => {} },
-        { text:'2', onClick: () => {} },
-        { text:'3', onClick: () => {} },
-        { text:'清空', onClick: () => {} },
-        { text: '4', onClick: () => { } },
-        { text: '5', onClick: () => { } },
-        { text: '6', onClick: () => { } },
-        { text: '+', onClick: () => { } },
-        { text: '7', onClick: () => { } },
-        { text: '8', onClick: () => { } },
-        { text: '9', onClick: () => { } },
-        { text: '-', onClick: () => { } },
-        { text: '.', onClick: () => { } },
-        { text: '0', onClick: () => { } },
-        { text: '删', onClick: () => { } },
-        { text: '提交', onClick: () => { } },
-    ]
-    return () => <>
-            <div class={s.dateAndAmount}>
-              <span class={s.date}>
-                <Icon name="date" class={s.icon}/>
-                <span>2022-01-01</span>
-              </span>
-              <span class={s.amount}>199.12368</span>
-            </div>
-            <div class={s.buttons}>
-            { buttons.map((button)=> 
-                <button onClick={button.onClick}>{button.text}</button> 
-            )}
-            </div>
-        </>
+      { text: "1", onClick: () => {} },
+      { text: "2", onClick: () => {} },
+      { text: "3", onClick: () => {} },
+      { text: "清空", onClick: () => {} },
+      { text: "4", onClick: () => {} },
+      { text: "5", onClick: () => {} },
+      { text: "6", onClick: () => {} },
+      { text: "+", onClick: () => {} },
+      { text: "7", onClick: () => {} },
+      { text: "8", onClick: () => {} },
+      { text: "9", onClick: () => {} },
+      { text: "-", onClick: () => {} },
+      { text: ".", onClick: () => {} },
+      { text: "0", onClick: () => {} },
+      { text: "删", onClick: () => {} },
+      { text: "提交", onClick: () => {} },
+    ];
+    return () => (
+      <>
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon name="date" class={s.icon} />
+            <span>
+              2022-01-01
+              <input type='date' value={time(now).format()}></input>
+            </span>
+          </span>
+          <span class={s.amount}>199.12368</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map((button) => (
+            <button onClick={button.onClick}>{button.text}</button>
+          ))}
+        </div>
+      </>
+    );
   },
 });

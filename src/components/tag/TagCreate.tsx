8a164c9fc928f0b3384627c02bd18b9f10c59503
ashine33 <404770266@qq.com>
2022-09-27 17:43:46 +0800

    make add tags CSS

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,53 +1,67 @@
 import { defineComponent, PropType } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
+import { Button } from "../../shared/Button";
+import s from "./TagCreate.module.scss";
 
 export const TagCreate = defineComponent({
   props: { name: { type: String as PropType<string> } },
   setup: (props, context) => {
-    return () => 
+    return () => (
       <MainLayout>
         {{
           title: () => "add tag",
           icon: () => <Icon name="left" onClick={() => {}} />,
           default: () => (
-            <form>
-              <div>
-                <label>
-                  <span>新建标签</span>
-                  <input></input>
-                </label>
-              </div>
-              <div>
-                <label>
-                  <span>符号</span>
-                  <div>
-                    <nav>
-                      <span>aa</span>
-                      <span>bb</span>
-                      <span>cc</span>
-                      <span>dd</span>
-                      <span>ee</span>
-                      <span>ff</span>
-                    </nav>
-                    <ol>
-                      <li>1</li>
-                      <li>2</li>
-                      <li>3</li>
-                    </ol>
+            <form class={s.form}>
+              <div class={s.formRow}>
+                <label class={s.formLabel}>
+                  <span class={s.formItem_name}>标签名</span>
+                  <div class={s.formItem_value}>
+                    <input class={[s.formItem, s.input, s.error]}></input>
+                  </div>
+                  <div class={s.formItem_errorHint}>
+                    <span>必填</span>
                   </div>
                 </label>
               </div>
-              <div>
-                <p>记账时长按标签即可编辑</p>
+              <div class={s.formRow}>
+                <label class={s.formLabel}>
+                  <span class={s.formItem_name}>符号</span>
+                  <div class={s.formItem_value}>
+                    <div class={[s.formItem, s.emojiList, s.error]}>
+                      <nav>
+                        <span class={s.selected}>表情</span>
+                        <span>手势</span>
+                        <span>职业</span>
+                        <span>衣服</span>
+                        <span>动物</span>
+                        <span>自然</span>
+                      </nav>
+                      <ol>
+                        <li>😀</li>
+                        <li>😀</li>
+                        <li>😀</li>
+                        <li>😀</li>
+                        <li>😀</li>
+                        <li>😀</li>
+                        <li>😀</li>
+                        <li>😀</li>
+                      </ol>
+                    </div>
+                  </div>
+                </label>
               </div>
-              <div>
-                <button>确定</button>
+              <p class={s.tips}>记账时长按标签即可进行编辑</p>
+              <div class={s.formRow}>
+                <div class={s.formItem_value}>
+                  <Button class={[s.formItem, s.button]}>确定</Button>
+                </div>
               </div>
             </form>
           ),
         }}
-      </MainLayout>;
-    ;
+      </MainLayout>
+    );
   },
 });

88f212b87ee6b9dbf974e54ebedfc57a65c20099
ashine33 <404770266@qq.com>
2022-09-29 15:23:40 +0800

    feat：创建 TagEdit.tsx

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -2,7 +2,7 @@ import { defineComponent, PropType, reactive, toRaw  } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Button } from "../../shared/Button";
-import s from "./TagCreate.module.scss";
+import s from "./Tag.module.scss";
 import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Rules, validate } from '../../shared/validate';
 

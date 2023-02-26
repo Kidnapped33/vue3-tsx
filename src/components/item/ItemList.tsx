e50658a3f73b619eba0ca5bdb66580d277638d5e
ashine33 <404770266@qq.com>
2023-02-26 21:41:46 +0800

    feat: 封装 TimeTabsLayout 4个Tab 切换组件

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -9,6 +9,7 @@ import { Overlay } from "vant";
 import { Form, FormItem } from "../../shared/Form";
 import { Button } from "../../shared/Button";
 import { OverlayIcon } from "../../shared/Overlay";
+import { TimeTabsLayout } from "../../layouts/TimeTabsLayout";
 
 export const ItemList = defineComponent({
   props: {
@@ -17,44 +18,6 @@ export const ItemList = defineComponent({
     },
   },
   setup: (props, context) => {
-    const refSelected = ref<string>("本月");
-    const time = new Time();
-    // const customTime = ref<string[]>([time.firstDayOfMonth().format(),time.lastDayOfMonth().format()])
-    const customTime = reactive({
-      start: new Time().format(),
-      end: new Time().format(),
-    });
-    const timeList = [
-      // '本月'
-      {
-        start: time.firstDayOfMonth(),
-        end: time.lastDayOfMonth(),
-      },
-      // '上月'
-      {
-        start: time.add(-1, "month").firstDayOfMonth(),
-        end: time.add(-1, "month").lastDayOfMonth(),
-      },
-      // 今年'
-      {
-        start: time.firstDayOfYear(),
-        end: time.lastDayOfYear(),
-      },
-    ];
-
-    // watchEffect(()=>{
-    //   if(refSelected.value === '自定义起始时间'){
-    //     refOverlayVisible.value = true
-    //   }
-    // })
-
-    const refOverlayVisible = ref<boolean>(false);
-    const onSubmitCustomTime = (e: Event) => {
-      console.log('eee',e.target)
-      e.preventDefault();
-      refOverlayVisible.value = false;
-    };
-
     return () => (
       <MainLayout>
         {{
@@ -62,64 +25,7 @@ export const ItemList = defineComponent({
           icon: () => <OverlayIcon />,
           default: () => (
             <>
-              <Tabs
-                classPrefix={"customTabs"}
-                v-model:selected={refSelected.value}
-                onUpdate:selected={() =>
-                  (refOverlayVisible.value =
-                    refSelected.value === "自定义起始时间")
-                }
-              >
-                <Tab name="本月">
-                  <ItemSummary
-                    startTime={timeList[0].start.format()}
-                    endTime={timeList[0].end.format()}
-                  />
-                </Tab>
-                <Tab name="上月">
-                  <ItemSummary
-                    startTime={timeList[1].start.format()}
-                    endTime={timeList[1].end.format()}
-                  />
-                </Tab>
-                <Tab name="今年">
-                  <ItemSummary
-                    startTime={timeList[2].start.format()}
-                    endTime={timeList[2].end.format()}
-                  />
-                </Tab>
-                <Tab name="自定义起始时间">
-                  <ItemSummary
-                    startTime={customTime.start}
-                    endTime={customTime.end}
-                  />
-                </Tab>
-              </Tabs>
-              <Overlay show={refOverlayVisible.value} class={s.overlay}>
-                <div class={s.overlay_inner}>
-                  <header>请选择时间</header>
-                  <main>
-                    <Form onSubmit={onSubmitCustomTime}>
-                      <FormItem
-                        label="开始时间"
-                        v-model={customTime.start}
-                        type="date"
-                      />
-                      <FormItem
-                        label="结束时间"
-                        v-model={customTime.end}
-                        type="date"
-                      />
-                      <FormItem>
-                        <div class={s.actions}>
-                          <button type="button" onClick={()=> refOverlayVisible.value = false}>取消</button>
-                          <button type="submit">确认</button>
-                        </div>
-                      </FormItem>
-                    </Form>
-                  </main>
-                </div>
-              </Overlay>
+              <TimeTabsLayout component={ ItemSummary } />
             </>
           ),
         }}

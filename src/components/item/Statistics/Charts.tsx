17e741e06514f66e2cdd621283154011d2f65a09
ashine33 <404770266@qq.com>
2023-02-27 19:49:27 +0800

    feat: 完成第三个图表

--- src/components/item/Statistics/Charts.tsx
+++ src/components/item/Statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, ref } from "vue";
+import { computed, defineComponent, onMounted, PropType, reactive, ref } from "vue";
 import { FormItem } from "../../../shared/Form";
 import s from "./Charts.module.scss";
 import * as echarts from "echarts";
@@ -18,8 +18,21 @@ export const Charts = defineComponent({
     const category = ref("expenses");
     const refLineChart = ref<HTMLDivElement>();
     const refPieChart = ref<HTMLDivElement>();
+    const data3 = reactive([
+      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
+      { tag: { id: 2, name: '吃饭', sign: 'x' }, amount: 1000 },
+      { tag: { id: 3, name: '娱乐', sign: 'x' }, amount: 900 },
+    ])
+
+    const betterData3 = computed(() => {
+      const total = data3.reduce((sum, item) => sum + item.amount, 0)
+      return data3.map(item => ({
+        ...item,
+        percent: Math.round(item.amount / total * 100) + '%'
+      }))
+    })
+
     onMounted(() => {
-      // if( refLineChart.value === undefined ){ return };
       let lineChart = echarts.init(refLineChart.value!);
       lineChart.setOption({
         grid: [
@@ -42,7 +55,6 @@ export const Charts = defineComponent({
     });
     
     onMounted(()=>{
-      // if( refPieChart.value === undefined ){ return };
       let pieChart = echarts.init(refPieChart.value!);
       pieChart.setOption({
         grid: [
@@ -84,6 +96,26 @@ export const Charts = defineComponent({
         />
          <div ref={refLineChart} class={s.lineChart}></div>
          <div ref={refPieChart} class={s.pieChart}></div>
+         <div class={s.demo3}>
+          {betterData3.value.map(({ tag, amount, percent }) => {
+            return (
+              <div class={s.topItem}>
+                <div class={s.sign}>
+                  {tag.sign}
+                </div>
+                <div class={s.bar_wrapper}>
+                  <div class={s.bar_text}>
+                    <span> {tag.name} - {percent} </span>
+                    <span> ￥{amount} </span>
+                  </div>
+                  <div class={s.bar}>
+                    <div class={s.bar_inner}></div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   },

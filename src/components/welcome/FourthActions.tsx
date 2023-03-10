09a34ebeb5d94dc392e015b44c4d60a45805ecbf
ashine33 <404770266@qq.com>
2023-03-11 01:21:45 +0800

    fix: 修改第四页样式

--- src/components/welcome/FourthActions.tsx
+++ src/components/welcome/FourthActions.tsx
@@ -6,7 +6,7 @@ export const FourthActions: FunctionalComponent = () => {
   return <div class={s.actions}>
     <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
     <RouterLink to="/start" >完成</RouterLink>
-    <RouterLink to="/start" >跳过</RouterLink>
+    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
   </div>
 }
 

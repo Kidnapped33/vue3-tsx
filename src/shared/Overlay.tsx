a73ebbe0290a3f1227ff557830cb65e4b4ce4293
ashine33 <404770266@qq.com>
2023-03-11 01:41:52 +0800

    feat: 可退出登录

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -13,14 +13,18 @@ export const Overlay = defineComponent({
       props.onClose?.();
     };
 
+    const token = ref<string>('123321')
+    token.value = JSON.stringify(localStorage.getItem('token'))
+    console.log('token', token.value)
+
     const onClickSignIn = () => {};
     return () => (
       <>
         <div class={s.mask} onClick={close}></div>
         <div class={s.overlay}>
           <section class={s.currentUser} onClick={onClickSignIn}>
-            <h2>未登录用户</h2>
-            <p>点击登录</p>
+            <h2>{token ? 'admin' : '未登录用户'}</h2>
+            <p><RouterLink to="/sign_in">{ token ? '退出登录' : '点击登录'}</RouterLink></p>
           </section>
           <nav>
             <ul class={s.action_list}>
@@ -31,17 +35,17 @@ export const Overlay = defineComponent({
                 </RouterLink>
               </li>
               <li>
-                <RouterLink to="/export" class={s.action}>
+                <RouterLink to="" class={s.action}>
                   <Icon name="export" class={s.icon} />
                   <span>导出数据</span>
                 </RouterLink>
               </li>
-              <li>
-                <RouterLink to="/notify" class={s.action}>
+              {/* <li>
+                <RouterLink to="" class={s.action}>
                   <Icon name="notify" class={s.icon} />
                   <span>记账提醒</span>
                 </RouterLink>
-              </li>
+              </li> */}
             </ul>
           </nav>
         </div>

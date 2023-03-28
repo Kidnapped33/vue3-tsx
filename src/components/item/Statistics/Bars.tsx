c1c807f14421dc21468832599faa3cc30d49743b
ashine33 <404770266@qq.com>
2023-03-29 01:31:35 +0800

    feat: 完成 bars 本月图标数据

--- src/components/item/Statistics/Bars.tsx
+++ src/components/item/Statistics/Bars.tsx
@@ -1,23 +1,34 @@
-import { computed, defineComponent, PropType, reactive } from "vue";
+import { computed, defineComponent, PropType, ref, watchEffect } from "vue";
 import s from "./Bars.module.scss";
 
 export const Bars = defineComponent({
   props:{
     data: {
-      type: Array,
+      type: Array as PropType<any[]>,
       required: true,
   },
 },
   setup: (props, context) => {
-    const data3 = reactive([
-      { tag: { id: 1, name: "房租", sign: "x" }, amount: 3000 },
-      { tag: { id: 2, name: "吃饭", sign: "x" }, amount: 1000 },
-      { tag: { id: 3, name: "娱乐", sign: "x" }, amount: 900 },
+
+   
+    const data3 = ref([
+      { tag: { id: 1, name: "房租", sign: "x" }, amount: 0 },
+      { tag: { id: 2, name: "吃饭", sign: "x" }, amount: 0 },
+      { tag: { id: 3, name: "娱乐", sign: "x" }, amount: 0 },
     ]);
 
+    watchEffect(() => {
+      data3.value = props.data.map(
+        (tagInfo) => {
+          // tagInfo
+          return { tag: { id: tagInfo.tag.id, name: tagInfo.tag.name , sign: tagInfo.tag.sign }, amount: tagInfo.amount };
+        }
+      )
+    });
+
     const betterData3 = computed(() => {
-      const total = data3.reduce((sum, item) => sum + item.amount, 0);
-      return data3.map((item) => ({
+      const total = data3.value.reduce((sum, item) => sum + item.amount, 0);
+      return data3.value.map((item) => ({
         ...item,
         percent: Math.round((item.amount / total) * 100) + "%",
       }));
@@ -38,7 +49,7 @@ export const Bars = defineComponent({
                   <span> ￥{amount} </span>
                 </div>
                 <div class={s.bar}>
-                  <div class={s.bar_inner}></div>
+                  <div class={s.bar_inner} style={{width:`${percent}`}}></div>
                 </div>
               </div>
             </div>

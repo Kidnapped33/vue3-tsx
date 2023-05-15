67a5e91b780987ce7a642341a3e612d8f3f3e27d
ashine33 <404770266@qq.com>
2023-05-15 13:23:21 +0800

    fix：日期显示为今天

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -161,7 +161,8 @@ export const InputPad = defineComponent({
               <span onClick={showDatePicker}>
                 {/* { new Time(refDate.value).format() } */}
                 {/* { new Time(refDate.value).firstDayOfMonth().format()} */}
-                {new Time(props.happenAt).firstDayOfMonth().format()}
+                {/* {new Time(props.happenAt).firstDayOfMonth().format()} */}
+                {new Time(props.happenAt).format()}
               </span>
               <Popup
                 position="bottom"
